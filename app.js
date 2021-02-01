df40449f6fb42971ec0b921c2f4dfda8cb245a3d
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-02-01 11:28:15 -0800

    event listener for home page to set initial user to local storage

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // import functions and grab DOM elements
+import { saveUser } from './utils.js';
 
 // initialize state
 
 // set event listeners to update state and DOM
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    const user = {
+        name: formData.get('userName'),
+        scores: [],
+    };
+    saveUser(user);
+    window.location = './game/index.html';
+});
