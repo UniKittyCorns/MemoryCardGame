3964713ec6cc2ba03b4c89c74944a8ed9e82218c
Soraya Benson <soraya.benson@gmail.com>
2021-02-02 11:51:20 -0800

    full deck commit

--- app.js
+++ app.js
@@ -38,5 +38,5 @@ form.addEventListener('submit', (e) => {
         saveUsers(existingUsers);
         setCurrentUser(currentUser);
     }
-    //window.location = './game/index.html';
+    window.location = './game/index.html';
 });
